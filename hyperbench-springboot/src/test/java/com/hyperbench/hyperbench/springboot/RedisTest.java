79d366bb2531fe3066ca9ab5a1ef78dc25402cff
H.wang.cheng079 <wang.cheng079@redstarclouds.com>
2020-08-20 18:01:13 +0800

    Redis

---
+++ hyperbench-springboot/src/test/java/com/hyperbench/hyperbench/springboot/RedisTest.java
@@ -0,0 +1,27 @@
+package com.hyperbench.hyperbench.springboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.CacheManager;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void test() throws Exception {
+        stringRedisTemplate.opsForValue().set("aaa", "111");
+        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+    }
+
+}
