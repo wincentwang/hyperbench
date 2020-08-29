4c7af4e4145fe6d8f62e5d8288e129e87f492b7c
H.wang.cheng079 <wang.cheng079@redstarclouds.com>
2020-08-29 13:02:22 +0800

    -- TEST

--- hyperbench-plantuml/src/main/java/com/hyperbenchh/hyperbench/plantuml/HashMapTest.java
+++ hyperbench-plantuml/src/main/java/com/hyperbenchh/hyperbench/plantuml/HashMapTest.java
@@ -1,5 +1,6 @@
 package com.hyperbenchh.hyperbench.plantuml;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,6 +20,11 @@ public class HashMapTest {
         test.put("6","2");
         test.put("7","2");
         test.put("8","2");
+
+        Date date = new Date();
+
+        boolean after = date.after(null);
+        System.out.println(after);
 
     }
 }
