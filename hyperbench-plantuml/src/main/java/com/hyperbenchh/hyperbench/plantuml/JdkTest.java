dae41bd6a3a2901392de2abe80bbe29a883e531a
H.wang.cheng079 <wang.cheng079@redstarclouds.com>
2020-09-03 18:40:07 +0800

    -- TEST

--- hyperbench-plantuml/src/main/java/com/hyperbenchh/hyperbench/plantuml/JdkTest.java
+++ hyperbench-plantuml/src/main/java/com/hyperbenchh/hyperbench/plantuml/JdkTest.java
@@ -6,19 +6,24 @@ import java.util.Calendar;
 public class JdkTest {
 
     public static void main(String[] args) {
+
         SimpleDateFormat sdf=new SimpleDateFormat("dd");
         Calendar instance = Calendar.getInstance();
-        System.out.print(sdf.format(instance.getTime())+",");
+        System.out.print(sdf.format(instance.getTime())+"+");
         for(int i=0;i<5000;i++){
             instance.add(Calendar.DAY_OF_MONTH,1);
 
-            if(instance.get(Calendar.DAY_OF_WEEK)==1){
-                System.out.print(sdf.format(instance.getTime()));
-                System.out.println();
-                System.out.println(",,,,,,");
-            }else{
+            int year = instance.get(Calendar.YEAR);
+            int month = instance.get(Calendar.MONTH)+1;
+            int weeks = instance.get(Calendar.WEEK_OF_YEAR);
 
-                System.out.print(sdf.format(instance.getTime())+",");
+            String split=instance.getActualMaximum(Calendar.DAY_OF_MONTH)==Integer.parseInt(sdf.format(instance.getTime()))?" ":"-";
+
+            if(instance.get(Calendar.DAY_OF_WEEK)==1){
+                System.out.println(sdf.format(instance.getTime())+ "("+   year+"-"+month+"-"+weeks+ ")");
+                System.out.print("## ");
+            }else{
+                System.out.print(sdf.format(instance.getTime())+split);
             }
         }
     }
