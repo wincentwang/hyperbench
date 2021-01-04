f05161f5fd32f14f16167a81bc3bc675b43e08ca
H.wang.cheng079 <wang.cheng079@redstarclouds.com>
2021-01-04 11:08:01 +0800

    -- TEST

--- hyperbench-plantuml/src/main/java/com/hyperbenchh/hyperbench/plantuml/SynchronizedTest.java
+++ hyperbench-plantuml/src/main/java/com/hyperbenchh/hyperbench/plantuml/SynchronizedTest.java
@@ -1,17 +1,27 @@
 package com.hyperbenchh.hyperbench.plantuml;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SynchronizedTest {
 
     public static void main(String[] args) {
-        Demo demo = new Demo();
-        demo.start();
-        for (;;){
-            synchronized (demo){
-                if(demo.isFlag()){
-                    System.out.println("Flag is True");
-                }
-            }
-        }
+
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+
+
+
+
+
+
+
+
+
     }
 //    因为某一个线程进入synchronized代码块前后，线程会获得锁，清空工作内存，从主内存拷贝共享变量最新的值到工作内存成为副本，执行代码，将修改后的副本的值刷新回主内存中，线程释放锁。
 
--- hyperbench-springboot/src/main/java/com/hyperbench/hyperbench/springboot/rest/HelloController.java
+++ hyperbench-springboot/src/main/java/com/hyperbench/hyperbench/springboot/rest/HelloController.java
@@ -1,7 +1,13 @@
 package com.hyperbench.hyperbench.springboot.rest;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -10,5 +16,10 @@ public class HelloController {
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
+    }
+
+
+    public static void main(String[] args) {
+
     }
 }
